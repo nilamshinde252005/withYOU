6a288e7c30c948e46be48c04c92f179c4fc52c27
nilamshinde252005 <nilamshinde252005@gmail.com>
2026-02-13 21:39:12 +0000

    Fix loginRoute.js

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,6 +1,6 @@
 const { userController, registerController } = require("../controllers/userController");
 const loginRoute = (app) => {
-  app.get("/login", userController);
+  app.post("/login", userController);
   app.post("/login", userController);
   app.post("/register", registerController);
 };
