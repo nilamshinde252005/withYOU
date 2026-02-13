ffd5e290cf1b7f93fa6433f4ba1e96bbefc49dff
nilamshinde252005 <nilamshinde252005@gmail.com>
2026-02-13 18:46:28 +0000

    Remove old root app version

--- middleware/checkGmailUser.js
+++
@@ -1,8 +0,0 @@
-function checkGmailUser(req,res,next){
-    const username=req.payload?.name;
-    if(!username.endsWith('@gmail.com')){
-        return res.status(403).json({ message: 'Only @gmail.com users allowed' });
-    }
-    next();
-}
-module.exports = { checkGmailUser };
